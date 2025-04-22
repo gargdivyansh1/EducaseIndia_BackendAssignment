b8cdb2f6f30a8a19df8a2646cb2072d47900d925
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-04-22 19:19:02 +0530

    solve the port issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
